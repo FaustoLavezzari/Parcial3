2d9b71c20da1b951a68bdcdbfd08149dd4510a4c
Fausto Lavezzari <faustolavezzari99@gmail.com>
2021-06-17 11:29:29 -0300

    .

--- Parcial3.cpp
+++ Parcial3.cpp
@@ -119,35 +119,21 @@ bool hayInterseccion(coordenada p1, coordenada q1, coordenada p2, coordenada q2)
 	int o2 = orientation(p1, q1, q2);
 	int o3 = orientation(p2, q2, p1);
 	int o4 = orientation(p2, q2, q1);
-
-	// General case
 	if (o1 != o2 && o3 != o4)
 		return true;
-
-	// Special Cases
-	// p1, q1 and p2 are colinear and p2 lies on segment p1q1
 	if (o1 == 0 && onSegment(p1, p2, q1)) return true;
-
-	// p1, q1 and q2 are colinear and q2 lies on segment p1q1
 	if (o2 == 0 && onSegment(p1, q2, q1)) return true;
-
-	// p2, q2 and p1 are colinear and p1 lies on segment p2q2
 	if (o3 == 0 && onSegment(p2, p1, q2)) return true;
-
-	// p2, q2 and q1 are colinear and q1 lies on segment p2q2
 	if (o4 == 0 && onSegment(p2, q1, q2)) return true;
-
-	return false; // Doesn't fall in any of the above cases 
+	return false;
 }
 
 int orientation(coordenada p, coordenada q, coordenada r)
 {
 	int val = (q.y - p.y) * (r.x - q.x) -
 		(q.x - p.x) * (r.y - q.y);
-
-	if (val == 0) return 0;  // colinear
-
-	return (val > 0) ? 1 : 2; // clock or counterclock wise
+	if (val == 0) return 0;
+	return (val > 0) ? 1 : 2;
 }
 
 bool onSegment(coordenada p, coordenada q, coordenada r)
@@ -235,6 +221,7 @@ Nodo* encontrarHamiltoniano(vector<vector<double>> matrizAdyacencia) {
 			Finalizados.push_back(inicio);
 		}
 	}
+
 	Nodo* aux = new Nodo (NULL,0,0);
 	for(int i = 0; i < Finalizados.size(); i++) {
 		for (int j = i + 1; j < Finalizados.size(); j++) {
